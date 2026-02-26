de26ded6302b179c807e980261e4afc0706783b2
SISI <schwermc@my.erau.edu>
2026-02-25 21:03:42 -0700

    Bouncy Balls pt.1

---
+++ Physics/Assets/Scripts/BallsFromTheSky.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class BallsFromTheSky : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float interval = 0.5f;
+
+    [SerializeField] float range = 4f;
+
+    private float nextBallTime = 0f;
+    private Transform objectStart;
+    private ObjectPooler objectPooler;
+
+    void Start()
+    {
+        nextBallTime = Time.time + interval;
+        objectStart = transform;
+        objectPooler = GetComponent<ObjectPooler>();
+    }
+
+    void Update()
+    {
+        if (Time.time > nextBallTime)
+        {
+            Vector3 position = new Vector3(Random.Range(-range, range) + objectStart.position.x, objectStart.position.y, Random.Range(-range, range) + objectStart.position.z);
+            NewBall(position);
+            nextBallTime = Time.time + interval;
+        }
+    }
+
+    void NewBall(Vector3 position)
+    {
+        //Instantiate(ballPrefab, position, Quaternion.identity);
+        GameObject ball = objectPooler.GetPooledObject();
+        if (ball != null)
+        {
+            ball.transform.position = position;
+            ball.transform.rotation = Quaternion.identity;
+            ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
